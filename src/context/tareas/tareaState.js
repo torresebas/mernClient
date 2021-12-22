d2d1defc54fc5bb3783f22c3cd27532188816166
sebastianto <sebastt.93@gmail.com>
2021-12-23 10:24:03 +1100

    432. obteniendo tareas

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -29,11 +29,17 @@ const TareaState = (props) => {
   // Crear las funciones
 
   // Obtener las tareas de un proyecto
-  const obtenerTareas = (proyectoId) => {
-    dispatch({
-      type: TAREAS_PROYECTO,
-      payload: proyectoId,
-    });
+  const obtenerTareas = async (proyecto) => {
+    try {
+      const resultado = await clienteAxios.get("/api/tareas", {params: {proyecto}})
+      console.log(resultado)
+      dispatch({
+        type: TAREAS_PROYECTO,
+        payload: resultado.data.tareas
+      });
+    } catch (error) {
+      console.log(error)
+    }
   };
 
   // Agregar tarea a un proyecto seleccionado
