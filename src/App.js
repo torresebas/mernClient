df79e30daf2da2c487b712ce6a967c48d453f8bd
sebastianto <sebastt.93@gmail.com>
2021-12-22 13:15:01 +1100

    375 Proteger Componentes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import AlertaState from "./context/alertas/alertaState";
 import AuthState from "./context/autenticacion/authState";
 import tokenAuth from './config/token'
 
+import RutaPrivada from './components/rutas/RutaPrivada'
+
 //Revisar si tennemos un token
 
 const token = localStorage.getItem('token')
@@ -28,7 +30,7 @@ function App() {
               <Switch>
                 <Route exact path="/" component={Login} />
                 <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-                <Route exact path="/proyectos" component={Proyectos} />
+                <RutaPrivada exact path="/proyectos" component={Proyectos} />
               </Switch>
             </Router>
           </AuthState>
